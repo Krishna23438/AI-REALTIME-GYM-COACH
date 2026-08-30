5e39587dfed525ed601fd6bf7fa280dedecad4f0
Delta Student <krishnagupta2402@gmail.com>
2026-08-30 23:09:23 +0530

    commit

--- main.py
+++ main.py
@@ -31,5 +31,21 @@ def main():
     if not workout_started:
       st.selectbox("Exercise",options=EXERCISE_OPTIONS,key="plan_exercise")
 
+      st.number_input("Sets",min_value=0, max_value=50,key="plan_sets",step=1)
+
+      st.number_input("Reps per Set",min_value=0, max_value=50,key="plan_reps",step=1)
+
+      st.markdown("")
+
+      start_session_button = st.button("Start Session", width="stretch",key="start_session_button")
+
+      if start_session_button:
+        st.session_state["workout_started"] = True
+        st.rerun()
+
+    else:
+      st.write("Workout Started!")
+
+
 if __name__ == "__main__":
   main()
