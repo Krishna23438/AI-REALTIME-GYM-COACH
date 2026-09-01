a2ff5d5bd4e8f5962cb2980ba02672a198aa8eb2
Delta Student <krishnagupta2402@gmail.com>
2026-09-01 16:27:10 +0530

    commit

--- services/persistence/exercise_reprository.py
+++ services/persistence/exercise_reprository.py
@@ -2,3 +2,19 @@ import sqlite3
 import streamlit as st
 import pathlib as Path
 
+_DB_PATH = str(Path(__file__).parent.parent.parent / "data.db")
+
+@st.cache_resource
+def _get_connection():
+  conn = sqlite3.connect(_DB_PATH,check_same_thread=False)
+  conn.row_factory = sqlite3.Row
+  return conn
+
+def init_db():
+  conn = _get_connection()
+
+
+  with conn:
+    conn.execute("""
+  
+    """)
