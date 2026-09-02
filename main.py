9097dae3e26689036783deac573be294edc0cd63
Delta Student <krishnagupta2402@gmail.com>
2026-09-02 14:25:47 +0530

    commit

--- main.py
+++ main.py
@@ -116,7 +116,21 @@ def main():
   st.markdown("### Real-time pose detection with proactive AI voice coaching")
 
   if not workout_started:
-       st.markdown("")
+       st.markdown("""
+          <div style="
+        border: 10px dashed #444;
+        border-radius: 0px;
+        padding: 48px 32px;
+        text-align: center;
+        color: #888;
+        margin-top: 32px;
+    ">
+        <h2 style="color: #ccc; margin-bottom: 8px;">Set your workout plan</h2>
+        <p style="font-size: 1.05rem;">Choose your exercise , sets and reps in the sidebar, <br>then click <strong>Start Workout</strong> to activate the camera and 
+    </p>
+  </div>
+
+      """,unsafe_allow_html=True)
      
 
 if __name__ == "__main__":
