832358b534c1e57dce02a811553a9e1753ec1f58
Delta Student <krishnagupta2402@gmail.com>
2026-09-02 20:26:15 +0530

    commit

---
+++ detectors/squat.py
@@ -0,0 +1,13 @@
+from core.base_exercise import BaseExercise
+
+class SquatDetector(BaseExercise):
+  
+  def __init__(self):
+    super().__init__()
+
+  def reset(self):
+    self.reps = 0 
+    self.stage = None
+
+  def process(self, landmarks):
+    return super().process(landmarks)
