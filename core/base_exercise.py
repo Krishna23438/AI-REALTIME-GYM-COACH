7a3827f5f1c8ad09ccdbaf5fc1fb4ad90abc9aa0
Delta Student <krishnagupta2402@gmail.com>
2026-09-02 19:37:22 +0530

    commit

--- core/base_exercise.py
+++ core/base_exercise.py
@@ -5,3 +5,11 @@ class BaseExercise(ABC):
   def __init__(self):
     self.reps = 0
     self.stage = None
+
+  @abstractmethod
+  def process(self,landmarks):
+    pass
+
+  @abstractmethod
+  def reset(self):
+    pass
