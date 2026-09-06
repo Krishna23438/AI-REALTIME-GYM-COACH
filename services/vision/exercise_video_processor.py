277e448cfa6863001c17dc3a210b9b532c2b7c85
Delta Student <krishnagupta2402@gmail.com>
2026-09-06 18:19:39 +0530

    commit

--- services/vision/exercise_video_processor.py
+++ services/vision/exercise_video_processor.py
@@ -1,5 +1,8 @@
+import os
 import threading
 from streamlit_webrtc import VideoProcessorBase
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 class VideoProcessorClass(VideoProcessorBase):
   def __init__(self):
@@ -7,4 +10,5 @@ class VideoProcessorClass(VideoProcessorBase):
       self._latest_matrics = None
       self.exercise_type = "Squats"
 
-      
+      model_path = os.path.join(os.getcwd(), "ml_models", "pose_landmark_full_task")
+      base_option = python.BaseOptions(model_path=model_path)
